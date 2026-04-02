3fee2b1bb32fed22a265d2de559911c01320329f
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2026-04-01 21:39:48 -0300

    Aula do dia 01/04/2026.

---
+++ Atividades/atividade06/src/com/atividade06/app/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+// TODO: atividade 06
+// A partir da superclasse abstrata Veiculo, crie as subclasses finais: 
+// Moto, Carro, Onibus e Caminhao, todas no package models. Instancie cada 
+// uma das subclasses na classe principal, e peça para o usuário informar 
+// os seguintes atributos privados de um veículo escolhido pelo próprio 
+// usuário: fabricante, modelo, cor, ano, placa. O  atributo privado 
+// categoria deverá ser informado pelo próprio programa. 
+// Diferenças entre as classes: carro terá bagageiro, onibus poderá ser 
+// leito ou não, e caminhao terá carroceria.
+// Ao final, o programa exibirá os dados do veículo escolhido por ele.
+// NOTE: utilize herança, abstração e encapsulamento para codar.
+// NOTE: divirtam-se 😎👌👍☕
